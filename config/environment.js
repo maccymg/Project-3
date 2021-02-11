6f4f7a848f702409905f92dc89cd11b6fb424e7c
Max <maxmorgangiles@gmail.com>
2021-02-11 15:34:18 +0000

    pre-deployment

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,7 @@
-export const port = 4000
-export const dbURI = 'mongodb://localhost/premier-league-players-db'
-export const secret = 'hello this is my secret'
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/premier-league-players-db'
+export const port = process.env.PORT || 4000
+export const secret = process.env.SECRET || 'shhhh its a secret'
 
